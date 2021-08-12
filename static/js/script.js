a35e63ec0315429f37c6bac7a4cfddbaf5b1338c
jaydavis359 <jaydavis_1@hotmail.com>
2021-08-12 22:30:05 +0000

    add script for modal function

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('select').formSelect();
-    
+    $('.modal').modal();
     
   });
 
